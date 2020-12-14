8023194287977b5f52ac578c667aefa95e9d9b50
David Schmitt <dschmitt.1992@googlemail.com>
2020-12-14 12:13:37 +0100

    fix variable names

--- evaluation/convert_runner.sh
+++ evaluation/convert_runner.sh
@@ -9,8 +9,8 @@ file2="bio_graph_list.txt"
 declare -A graphs
 graphs=(["biological"]='cost_matrix_component_nr_1_size_9_cutoff_10.0.cm')
 
-paste -d@ file1 file2 | while IFS="@" read -r f1 f2
+paste -d@ $file1 $file2 | while IFS="@" read -r f1 f2
 do
-  python3 convert_cm_to_metis.py "../../input/biological/cm_files/${graph}" "../../input/biological/graphs/bio-nr-1-size-9.graph" &
+  python3 convert_cm_to_metis.py "../../input/biological/cm_files/${f1}" "../../input/biological/graphs/${f2}" &
   wait
 done
