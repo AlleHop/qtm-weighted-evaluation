1f9f8194d7fcb629c14ce1e48c7cd06ef763753a
David Schmitt <dschmitt.1992@googlemail.com>
2020-12-14 11:28:17 +0100

    add list of all bio graphs

---
+++ evaluation/convert_runner.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -ex
+graph_sets='biological'
+file1="bio_cm_files_list.txt"
+file2="bio_graph_list.txt"
+
+
+declare -A graphs
+graphs=(["biological"]='cost_matrix_component_nr_1_size_9_cutoff_10.0.cm')
+
+paste -d@ file1 file2 | while IFS="@" read -r f1 f2
+do
+  python3 convert_cm_to_metis.py "../../input/biological/cm_files/${graph}" "../../input/biological/graphs/bio-nr-1-size-9.graph" &
+  wait
+done
