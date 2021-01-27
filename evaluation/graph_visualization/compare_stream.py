efe5d98f46c819f238312f0cda6c1b5633f19996
David Schmitt <dschmitt.1992@googlemail.com>
2021-01-27 15:54:56 +0100

    change gephi to use graph nr 2666

--- evaluation/graph_visualization/compare_stream.py
+++ evaluation/graph_visualization/compare_stream.py
@@ -1,16 +1,16 @@
 from networkit import *
 
-setSeed(4, False)
+setSeed(0, False)
 
 #G = readGraph("../../networkit/input/karate.graph", Format.METIS)
-G = readGraph("../../input/biological/graphs/bio-nr-3373-size-17.graph", Format.METIS)
-with open("../../input/biological/weights/bio-nr-3373-size-17.csv", 'r') as read_obj:
+G = readGraph("../../input/biological/graphs/bio-nr-2666-size-20.graph", Format.METIS)
+with open("../../input/biological/weights/bio-nr-2666-size-20.csv", 'r') as read_obj:
     # pass the file object to reader() to get the reader object
     # csv_reader = reader(read_obj)
     # Pass reader object to list() to get a list of lists
     weightMatrix = [list(map(int,rec)) for rec in csv.reader(read_obj, delimiter=',')]
 G.indexEdges()
-mover = community.QuasiThresholdEditingLocalMover(G, 2, 400, True, True, 100, True, 1, 1, weightMatrix)
+mover = community.QuasiThresholdEditingLocalMover(G, 0, 400, True, True, 100, True, 1, 1, weightMatrix)
 mover.run()
 Q = mover.getQuasiThresholdGraph()
 client = gephi.streaming.GephiStreamingClient(url='http://localhost:8080/workspace0')
@@ -25,20 +25,20 @@ communities = community.detectCommunities(Q)
 client.exportNodeValues(G, communities, "community")
 
 #G = readGraph("../../networkit/input/karate.graph", Format.METIS)
-H = readGraph("../../input/optimization/graphs/bio-nr-3373-size-17-opt.graph", Format.METIS)
-with open("../../input/optimization/weights/bio-nr-3373-size-17-opt.csv", 'r') as read_obj:
+H = readGraph("../../input/optimization/graphs/bio-nr-2666-size-20-opt.graph", Format.METIS)
+with open("../../input/optimization/weights/bio-nr-2666-size-20-opt.csv", 'r') as read_obj:
     # pass the file object to reader() to get the reader object
     # csv_reader = reader(read_obj)
     # Pass reader object to list() to get a list of lists
     weightMatrix = [list(map(int,rec)) for rec in csv.reader(read_obj, delimiter=',')]
 H.indexEdges()
-mover = community.QuasiThresholdEditingLocalMover(H, 0, 400, True, True, 100, True, 1, 1, weightMatrix)
-mover.run()
-QOpt = mover.getQuasiThresholdGraph()
+mover2 = community.QuasiThresholdEditingLocalMover(H, 0, 400, True, True, 100, True, 1, 1, weightMatrix)
+mover2.run()
+QOpt = mover2.getQuasiThresholdGraph()
 clientOpt = gephi.streaming.GephiStreamingClient(url='http://localhost:8080/workspace1')
 clientOpt.exportGraph(H)
-edits = mover.getRunningInfo()[b'edits']
-editsCost = mover.getRunningInfo()[b'edits_weight']
+edits = mover2.getRunningInfo()[b'edits']
+editsCost = mover2.getRunningInfo()[b'edits_weight']
 
 print("EditsOpt: ", edits)
 print("EditCostsOpt: ", editsCost)
